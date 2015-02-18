5cd5b59fee1645acf1779960683572d121d05568
Nikolay <mellonkhv@gmail.com>
2015-02-18 23:27:37 +1000

    VeganMatch3
    3 в ряд в openfl

---
+++ VeganMatch3/src/veganmatch3/VeganMatch3.hx
@@ -0,0 +1,21 @@
+package veganmatch3;
+
+import openfl.display.Sprite;
+import openfl.Lib;
+
+/**
+ * ...
+ * @author Mellonkhv
+ */
+
+class VeganMatch3 extends Sprite 
+{
+
+	public function new() 
+	{
+		super();
+		
+		// Assets:
+		// openfl.Assets.getBitmapData("img/assetname.jpg");
+	}
+}
